18691b4d51447d3373de4c582a1d7ead48f3d96f
ralmnsk <ralmnsk@gmail.com>
2020-05-29 15:25:22 +0300

    fix dependencies and libs file

--- api/src/main/java/App.java
+++ api/src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        boolean allPositiveNumbers = Utils.isAllPositiveNumbers("12", "79",null);
+        boolean allPositiveNumbers = Utils.isAllPositiveNumbers("12", "79");
         System.out.println(allPositiveNumbers);
     }
 }
